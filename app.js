691885fcaf144ae33789becbe222786bf98cf2a4
Lily <liiy120530@gmail.com>
2021-08-06 19:53:31 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,42 @@
+//建立Express
+const express = require('express')
+const app = express()
+const port = 3000
+
+//建立express-handlebars
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+
+//設定樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//設定靜態檔案
+app.use(express.static('public'))
+
+
+
+//route setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})

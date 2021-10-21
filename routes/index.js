55284ae3b52e27fd63aeb10021f00c9f097d35e3
Lily <liiy120530@gmail.com>
2021-10-21 21:53:58 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,18 @@ const home = require('./modules/home')
 
 // 引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
-
 //引入search
 const search = require('./modules/search')
 //引入sort
 const sort = require('./modules/sort')
+//引入 users
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router

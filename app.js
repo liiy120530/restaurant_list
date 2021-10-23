0faab32eeca2d81167e2e970a9199b6f218f4867
Lily <liiy120530@gmail.com>
2021-10-23 10:55:07 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -44,6 +44,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
+
+
 //將request導入路由器
 app.use(routes)
 
